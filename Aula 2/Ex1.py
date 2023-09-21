8c7e0b28c5bbe6de5beb0f57f026b16abac31b3b
VMTG <vitormtg02@ua.pt>
2023-09-21 23:43:41 +0100

    Exercicio 1

---
+++ Aula 2/Ex1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+#define functions here....
+
+from colorama import Fore, Back, Style
+
+maximum_number = 500
+
+def getDividers(value):
+    
+    
+    
+    
+    
+    return []
+
+
+def isPerfect(value):
+
+
+    return False
+
+
+
+
+
+
+def main():
+    #oasd
+
+if __name__ == "__main__":
+    main()
