d3d61fb7a8b49ce4de8793ac31fe7998b5a1d88d
VMTG <vitormtg02@ua.pt>
2023-09-21 20:54:12 +0100

    Exercicios da aula 1

---
+++ Aula 1/Ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+#define functions here....
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ Aula 1/Ex2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#shebang line to inform the OS that the content is in python
+
+#define functions here....
+
+maximum_number = 10
+
+def isPrime(value):
+
+    for i in range (2,value):
+        if value%i == 0:
+            print ('the number ' + str(value) + ' is not prime because we can divide by ' + str(i))
+            return False
+    
+    return True
+
+def main():
+    print("Starting to compute prime numbers up to " + str(maximum_number))
+    
+    for i in range (0, maximum_number):
+        if isPrime(i):
+            print('Number ' + str(i) + ' is prime.')
+        else:
+            print('Number ' + str(i) + ' is not prime.')
+
+
+if __name__ == "__main__":
+    main()
