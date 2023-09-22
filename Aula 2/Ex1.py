ba28ebd6f0dfd4348b67735ef34e6820c150b2f9
VMTG <vitormtg02@ua.pt>
2023-09-22 11:25:45 +0100

    Exercicio 1

--- Aula 2/Ex1.py
+++ Aula 2/Ex1.py
@@ -8,26 +8,37 @@ from colorama import Fore, Back, Style
 maximum_number = 500
 
 def getDividers(value):
-    
-    
-    
-    
-    
-    return []
+    """Gets a list of integer dividers of number value
 
+    Args:
+        value: number to get dividers.
+
+    Returns:
+        bool: is perfect (True) or not (False)
+    """
+    
+    dividers = []
+
+    for i in range (1,value):
+        if value%i==0:
+            dividers.append(i)
+
+    return dividers
 
 def isPerfect(value):
 
+    dividers = getDividers(value)
 
-    return False
-
-
-
-
+    return value == sum(dividers)
 
 
 def main():
-    #oasd
+    print("Starting to compute perfect numbers up to" + str(maximum_number))
 
-if __name__ == "__main__":
-    main()
+    for i in range(1, maximum_number):
+        if isPerfect(i):
+            print('Number ' + str(i) + ' is Perfect.')
+
+
+    if __name__ == "__main__":
+        main()
